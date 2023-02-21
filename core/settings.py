d029721f6978df9bf68592675235c0c07abbbd2e
Fahmida Sultana Mitu <80353930+FahmidaMitu@users.noreply.github.com>
2023-02-21 18:36:04 +0600

    chnge

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(pjv5odapw51e!fu@fz-1#-cwrk-y%ux(g$*ahzz4g5cs$j7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [petrolpumpmanagementproject-production.up.railway.app]
 
 
 # Application definition
